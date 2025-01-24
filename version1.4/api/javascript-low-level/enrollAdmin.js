d5213acfe0203e119f3e77ae2c184365ddf83a39
gautam.atre <gautam.atre@htieshi.com>
2025-01-24 13:07:16 +0530

    new api js low level

---
+++ version1.4/api/javascript-low-level/enrollAdmin.js
@@ -0,0 +1,81 @@
+'use strict';
+/*
+* Copyright IBM Corp All Rights Reserved
+*
+* SPDX-License-Identifier: Apache-2.0
+*/
+/*
+ * Enroll the admin user
+ */
+
+let Fabric_Client = require('fabric-client');
+let Fabric_CA_Client = require('fabric-ca-client');
+
+let fs = require('fs');
+let path = require('path');
+let firstnetwork_path = '/home/gautam/blockchain/artys_blockchain/artifacts/channel';
+let org1tlscacert_path = '/home/gautam/blockchain/artys_blockchain/artifacts/channel/crypto-config/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem'
+
+
+
+//let firstnetwork_path = path.resolve('..', '..', 'first-network');
+//let org1tlscacert_path = path.resolve(firstnetwork_path, 'crypto-config', 'peerOrganizations', 'org1.example.com', 'tlsca', 'tlsca.org1.example.com-cert.pem');
+let org1tlscacert = fs.readFileSync(org1tlscacert_path, 'utf8');
+
+//
+let fabric_client = new Fabric_Client();
+let fabric_ca_client = null;
+let admin_user = null;
+let store_path = path.join(__dirname, 'hfc-key-store');
+console.log(' Store path:'+store_path);
+
+// create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
+Fabric_Client.newDefaultKeyValueStore({ path: store_path
+}).then((state_store) => {
+    // assign the store to the fabric client
+    fabric_client.setStateStore(state_store);
+    let crypto_suite = Fabric_Client.newCryptoSuite();
+    // use the same location for the state store (where the users' certificate are kept)
+    // and the crypto store (where the users' keys are kept)
+    let crypto_store = Fabric_Client.newCryptoKeyStore({path: store_path});
+    crypto_suite.setCryptoKeyStore(crypto_store);
+    fabric_client.setCryptoSuite(crypto_suite);
+    let	tlsOptions = {
+    	trustedRoots: [org1tlscacert],
+    	verify: false
+    };
+    // be sure to change the http to https when the CA is running TLS enabled
+    fabric_ca_client = new Fabric_CA_Client('https://localhost:7054', tlsOptions , 'ca.org1.example.com', crypto_suite);
+
+    // first check to see if the admin is already enrolled
+    return fabric_client.getUserContext('admin', true);
+}).then((user_from_store) => {
+    if (user_from_store && user_from_store.isEnrolled()) {
+        console.log('Successfully loaded admin from persistence');
+        admin_user = user_from_store;
+        return null;
+    } else {
+        // need to enroll it with CA server
+        return fabric_ca_client.enroll({
+          enrollmentID: 'admin',
+          enrollmentSecret: 'adminpw'
+        }).then((enrollment) => {
+          console.log('Successfully enrolled admin user "admin"');
+          return fabric_client.createUser(
+              {username: 'admin',
+                  mspid: 'Org1MSP',
+                  cryptoContent: { privateKeyPEM: enrollment.key.toBytes(), signedCertPEM: enrollment.certificate }
+              });
+        }).then((user) => {
+          admin_user = user;
+          return fabric_client.setUserContext(admin_user);
+        }).catch((err) => {
+          console.error('Failed to enroll and persist admin. Error: ' + err.stack ? err.stack : err);
+          throw new Error('Failed to enroll admin');
+        });
+    }
+}).then(() => {
+    console.log('Assigned the admin user to the fabric client ::' + admin_user.toString());
+}).catch((err) => {
+    console.error('Failed to enroll admin: ' + err);
+});
